59fbe57b164452215f27e4a5ed950561857ee409
Jariver <jariver_2009@hotmail.com>
2019-12-02 16:17:40 +0800

    desktops

--- process.py
+++ process.py
@@ -45,14 +45,6 @@ def create_tuples(path, file, attr_dict):
     return tuples
 
 
-def write_data(out_path, out_file, tuples):
-    with open(out_path+out_file, mode='w', encoding='utf-8') as writer:
-        for tuple in tuples:
-            for value in tuple.value_dict.values():
-                writer.write(value + ' ')
-            writer.write('\n')
-
-
 def compute_freq(tuples):
     r"""Compute the frequency of all values.
 
@@ -141,7 +133,7 @@ def mark_label(true_str, false_str, tuples):
                 tup.label = 1
                 break
 
-    false_cid = list(map(int, false_str.split('.')))
+    false_cid = list(map(int, false_str.split(',')))
     for cid in false_cid:
         for tup in tuples:
             if tup.cid == cid:
@@ -176,7 +168,7 @@ def select_tuple(tuples, k):
     # false_str = input("Which tuple don't violate the CFDs you want to express? "
     #                    "(Please input the cid of tuples, e.g. 2,3,4,5) >>> ")
     true_str = '0,1'
-    false_str = '3'
+    false_str = '2,3,4,7'
 
     mark_label(true_str, false_str, tuples)
 
@@ -216,8 +208,11 @@ def generate_tup_predicate(dataset_list, dict_predicate, all_predicate_set):
     """
     predicate_list = list()
     predicate_list_minus = list()
+    predicate_list_0 = list()
+
     for dataset in dataset_list:
         tup_predicate_set = set()
+        tup_predicate_set_0 = set()
 
         if dataset.label == 1:
             for index, value in enumerate(dataset.feature_vec):
@@ -226,32 +221,26 @@ def generate_tup_predicate(dataset_list, dict_predicate, all_predicate_set):
             predicate_list.append(tup_predicate_set)
             tup_predicate_set_minus = all_predicate_set - tup_predicate_set
             predicate_list_minus.append(tup_predicate_set_minus)
-    return predicate_list, predicate_list_minus
+
+        if dataset.label == 0:
+            for index, value in enumerate(dataset.feature_vec):
+                if value == 1:
+                    tup_predicate_set_0.add(dict_predicate[index])
+            predicate_list_0.append(tup_predicate_set_0)
+
+    return predicate_list, predicate_list_minus, predicate_list_0
 
 
-def generate_R_CFDs(dataset_list, dict_predicate, predicate_list, predicate_list_minus, all_predicate_set):
+def generate_R_CFDs(predicate_list, predicate_list_minus):
     r"""
 
     Args:
-        dataset_list:
-        dict_predicate:
         predicate_list:
         predicate_list_minus:
-        all_predicate_set:
 
     Return:
     """
     R_CFDs = list()
-    for dataset in dataset_list:
-        tup_predicate_set = set()
-
-        if dataset.label == 1:
-            for index, value in enumerate(dataset.feature_vec):
-                if value == 1:
-                    tup_predicate_set.add(dict_predicate[index])
-            predicate_list.append(tup_predicate_set)
-            tup_predicate_set_minus = all_predicate_set - tup_predicate_set
-            predicate_list_minus.append(tup_predicate_set_minus)
 
     for predicate, predicate_minus in zip(predicate_list, predicate_list_minus):
         predicate = list(predicate)
@@ -259,11 +248,77 @@ def generate_R_CFDs(dataset_list, dict_predicate, predicate_list, predicate_list
         for rhs in predicate_minus:
             for i in range(1, 2 ** predicate_size):
                 CFD = dict()
-                LHS = set()
+                LHS = list()
                 for j in range(predicate_size):
                     if (i >> j) % 2 == 1:
-                        LHS.add(predicate[j])
+                        LHS.append(predicate[j])
                 CFD['LHS'] = LHS
                 CFD['RHS'] = rhs
                 R_CFDs.append(CFD)
     return R_CFDs
+
+
+def remove_from_R_CFDs(R_CFDs, predicate_list_0):
+    r"""
+
+    Args:
+        R_CFDs:
+        predicate_list_0:
+
+    Return:
+
+    """
+    for CFD in reversed(R_CFDs):
+        for predicate in predicate_list_0:
+            if all(pre in predicate for pre in CFD['LHS']):
+                if CFD['RHS'] in predicate:
+                    R_CFDs.remove(CFD)
+
+
+def clean_from_R_CFDs(R_CFDs):
+    r"""
+
+    Args:
+        R_CFDs:
+    """
+    R_CFDs.sort(key=lambda e: (e['RHS'], e['LHS']), reverse=True)
+
+    for CFD in reversed(R_CFDs):
+        for pre in CFD['LHS']:
+            rhs = list(map(str, CFD['RHS'].split('=')))
+            if rhs[0] in pre:
+                R_CFDs.remove(CFD)
+                continue
+
+    rhs_dict = dict()
+    for CFD in reversed(R_CFDs):
+        rhs = CFD['RHS']
+        if rhs not in rhs_dict.keys():
+            rhs_dict[rhs] = set()
+        for pre in CFD['LHS']:
+            if pre in rhs_dict[rhs]:
+                R_CFDs.remove(CFD)
+                break
+            rhs_dict[rhs].add(pre)
+    R_CFDs.sort(key=lambda e: (e['LHS'], e['RHS']))
+
+
+def output(R_CFDs, out_path, out_file):
+    r"""
+
+    Args:
+        R_CFDs:
+        out_path:
+        out_file:
+    """
+    with open(out_path + out_file, mode='w', encoding='utf-8') as writer:
+        for CFD in R_CFDs:
+            flag = 0
+            for pre in CFD['LHS']:
+                if flag == 0:
+                    writer.write('(' + pre)
+                    flag = 1
+                else:
+                    writer.write(', ' + pre)
+            writer.write(') => ' + CFD['RHS'])
+            writer.write('\n')
--- run.py
+++ run.py
@@ -8,11 +8,10 @@ out_path = './output'
 out_file = '/output.txt'
 
 delta = 3
-k = 3
+k = 6
 
 attr_dict, attr_dict_re = process.create_attr_dict(path, file)
 tuples = process.create_tuples(path, file, attr_dict)
-process.write_data(out_path, out_file, tuples)
 dict_freq = process.compute_freq(tuples)
 dict_predicate, dict_predicate_re = process.compute_predicate(dict_freq, delta)
 process.compute_feature_vector(tuples, dict_predicate, dict_predicate_re)
@@ -20,9 +19,12 @@ process.compute_confidence(tuples)
 process.tup_sort(tuples)
 dataset_list = process.select_tuple(tuples, k)
 all_predicate_set = process.generate_all_predicate(dict_predicate_re)
-predicate_list, predicate_list_minus = process.generate_tup_predicate(dataset_list, dict_predicate, all_predicate_set)
-R_CFDs = process.generate_R_CFDs(dataset_list, dict_predicate, predicate_list, predicate_list_minus, all_predicate_set)
+predicate_list, predicate_list_minus, predicate_list_0 = process.generate_tup_predicate(dataset_list, dict_predicate, all_predicate_set)
+R_CFDs = process.generate_R_CFDs(predicate_list, predicate_list_minus)
 
+process.remove_from_R_CFDs(R_CFDs, predicate_list_0)
+process.clean_from_R_CFDs(R_CFDs)
+process.output(R_CFDs, out_path, out_file)
 # training_set, labels = trainingset.generate_training_set(tuples)
 # print(training_set, labels)
 # train.train(training_set, labels)
